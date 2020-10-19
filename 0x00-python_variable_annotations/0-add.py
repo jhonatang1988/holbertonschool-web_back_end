060c35bb2f5c34fa4f3f674bb21286b05b40bb3a
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 17:47:09 -0500

    0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""
+add two floats with type checking
+"""
+
+
 def add(a: float, b: float) -> float:
+    """ add two floats with type checking """
     return float(a + b)
