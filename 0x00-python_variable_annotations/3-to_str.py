6c50c6109d260861f72e0e2db1700d612d11a558
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 17:59:14 -0500

    3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+takes a float and returns his string repr
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float and returns his string repr
+    :param n: float
+    :return: str repr of n
+    """
+    return str(n)
