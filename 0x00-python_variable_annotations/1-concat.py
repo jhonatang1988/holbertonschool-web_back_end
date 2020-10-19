0a98c7f484352906bf384983df4ebed0b4f64679
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 17:52:43 -0500

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+string concatenations with type annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    string concatenations with type annotations
+    :param str1: str1
+    :param str2: str2
+    :return: concat of str1 and str2
+    """
+    return str1 + str2
