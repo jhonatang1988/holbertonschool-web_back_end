85a2b476a8363dfe8b7060c4547bb12b6b467b84
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 17:56:01 -0500

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+takes to floats and returns floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    takes to floats and returns floor
+    :param n: float to floor
+    :return: floor of n
+    """
+    return math.floor(n)
