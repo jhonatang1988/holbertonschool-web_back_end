f64f748f1cf666d0bafd336723d8fe21db625886
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 20:52:30 -0500

    11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+type variables for generic functions
+"""
+from typing import Mapping, TypeVar, Any, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    type variables for generic functions
+    :param dct: a mapping of anything
+    :param key: anything
+    :param default: key or None
+    :return: Anything or VT
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
