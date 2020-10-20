1a653c1e334fc47d34ebdc5ac09186c2a7e6f1d7
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-19 21:03:41 -0500

    12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+correct code to comply with mypy
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
+"""
+from typing import List, Any, Union
+
+
+def zoom_array(lst: List[Any], factor: Union[int, float] = 2) -> List[Any]:
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
