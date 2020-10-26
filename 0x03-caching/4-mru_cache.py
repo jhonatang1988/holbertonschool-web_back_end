a6b90030cf3c991583769b10148cd9abc5e90a25
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-26 17:39:59 -0500

    4

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+MRU caching exercise
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU cache eviction system
+    """
+
+    def __init__(self):
+        """
+        call parents
+        """
+        BaseCaching.__init__(self)
+        self.order = []
+
+    def put(self, key, item):
+        """
+        put a value in cache
+        :param key: key
+        :param item: value
+        :return: None
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+            if key in self.order:
+                self.order.remove(key)
+            self.order.append(key)
+
+            if len(self.order) > BaseCaching.MAX_ITEMS:
+                last = self.order.pop(-2)
+                self.cache_data.pop(last)
+                print('DISCARD: {}'.format(last))
+
+    def get(self, key):
+        """
+        get a value from cache
+        :param key: key
+        :return: value or None
+        """
+
+        if key in self.order:
+            self.order.remove(key)
+            self.order.append(key)
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
