1e6b78c55ae14183af8549639a3a3de5d16a6cac
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-26 16:48:19 -0500

    1

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,51 +1,72 @@
 #!/usr/bin/python3
 """
-FIFO caching exercise
+    BaseCache module
 """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    fifo cache eviction system
+    """ FIFOCache define a FIFO algorithm to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+
+      Ex:
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Holberton", D: "School"}
+      >>> my_cache.put("C", "Street")
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Street", D: "School"}
+
+      >>> my_cache.put("F", "COD")
+      DISCARD: A
+      >>> print(self.cache_data)
+      {F: "COD", B: "World", C: "Holberton", D: "School"}
     """
 
     def __init__(self):
-        """
-        call parents
+        """ Initiliaze
         """
         super().__init__()
-        self.order = []
 
     def put(self, key, item):
         """
-        put a value in cache
-        :param key: key
-        :param item: value
-        :return: None
+            modify cache data
+
+            Args:
+                key: of the dict
+                item: value of the key
         """
-        if key is None or item is None:
-            pass
-        else:
+        if key or item is not None:
+            valuecache = self.get(key)
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = list(self.cache_data.keys())[0]
+                    del self.cache_data[keydel]
+                    print("DISCARD: {}".format(keydel))
+
             self.cache_data[key] = item
-
-            if key in self.order:
-                self.order.remove(key)
-            self.order.append(key)
-
-            if len(self.order) > BaseCaching.MAX_ITEMS:
-                last = self.order.pop(0)
-                self.cache_data.pop(last)
-                print(f'DISCARD: {last}')
 
     def get(self, key):
         """
-        get a value from cache
-        :param key: key
-        :return: value or None
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
         """
-        try:
-            return self.cache_data[key]
-        except KeyError:
-            return None
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
