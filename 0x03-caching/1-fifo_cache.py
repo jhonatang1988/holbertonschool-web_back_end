3d76811628ff644ccaac272fe49f213e9d8d614e
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-10-26 16:58:59 -0500

    1

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -3,20 +3,42 @@
 FIFO caching exercise
 """
 
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    fifo cache eviction system
+    """ FIFOCache define a FIFO algorithm to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+
+      Ex:
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Holberton", D: "School"}
+      >>> my_cache.put("C", "Street")
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Street", D: "School"}
+
+      >>> my_cache.put("F", "COD")
+      DISCARD: A
+      >>> print(self.cache_data)
+      {F: "COD", B: "World", C: "Holberton", D: "School"}
     """
 
     def __init__(self):
-        """
-        call parents
+        """ Initiliaze
         """
         super().__init__()
-        # self.order = []
+        self.order = []
 
     def put(self, key, item):
         """
@@ -30,16 +52,16 @@ class FIFOCache(BaseCaching):
         else:
             self.cache_data[key] = item
 
-            # if key in self.order:
-            #     self.order.remove(key)
-            # self.order.append(key)
+            if key in self.order:
+                self.order.remove(key)
+            self.order.append(key)
 
             if len(self.order) > BaseCaching.MAX_ITEMS:
-                # last = self.order.pop(0)
-                # self.cache_data.pop(last)
-                # print(f'DISCARD: {last}')
-                self.cache_data.pop()
-                print(f'DISCARD:')
+                last = self.order.pop(0)
+                self.cache_data.pop(last)
+                print(f'DISCARD: {last}')
+                # self.cache_data.pop()
+                # print(f'DISCARD:')
 
     def get(self, key):
         """
